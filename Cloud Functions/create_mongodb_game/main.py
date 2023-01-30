17f24f0da086335ac06aaf98035dd9e2459a05d5
Alex <40860289+AJVEATS@users.noreply.github.com>
2023-01-30 10:05:38 +0000

    Update main.py

--- Cloud Functions/create_mongodb_game/main.py
+++ Cloud Functions/create_mongodb_game/main.py
@@ -8,7 +8,7 @@ import json
 
 def create_mongodb_single_game(request):
     cluster = MongoClient(
-        "mongodb+srv://AJVEATS:alexTest@gamestoredb.gaxxodr.mongodb.net/?retryWrites=true&w=majority"
+#         "mongodb+srv://AJVEATS:alexTest@gamestoredb.gaxxodr.mongodb.net/?retryWrites=true&w=majority"
     )
     db = cluster["GameStoreDB"]
     collection = db["Games"]
